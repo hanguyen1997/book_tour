7cb2d630527c14ff76b6ac70af3f5dc9cadd6890
hanguyen1997 <hanguyen1997@gmail.com>
2019-04-09 17:36:08 +0700

    update do du lieu

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,9 +1,6 @@
 class ToursController < ApplicationController
-  def index
-    if params[:filter] == "tours_domestic"
+  def index 
       @tours_domestic = Tour.tours_domestic(1)
-    else
       @tours_international = Tour.tours_international(2)
-    end
   end
 end
