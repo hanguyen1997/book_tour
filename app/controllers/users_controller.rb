0980cc7e1eca1add730e9416e17fec288b414c86
hanguyen1997 <hanguyen1997@gmail.com>
2019-04-13 14:04:16 +0700

    chinh sua thong tin khach hang

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:index, :edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -14,16 +15,42 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Welcome to viet travel"
-      redirect_to @user
+      redirect_to current_user
     else
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to current_user
+    else
+      render "edit"
     end
   end
 
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+      params.require(:user).permit(:name, :email,:password, :password_confirmation, :address, :phone_number)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless @user == current_user
     end
 end
