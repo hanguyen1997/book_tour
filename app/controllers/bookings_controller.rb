d5d5288875bcffe4e6b8e4b562100496e478dbee
hanguyen1997 <truongbahanguyen@gmail.com>
2019-04-29 17:48:04 +0700

    hoan thanh new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,23 @@
 class BookingsController < ApplicationController
 
   def new 
+  	 @description_detail = DescriptionDetail.find_by id: params[:id]
+  	 @booking = Booking.new
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+	def booking_params
+	  params.require(:booking).permit(:user_id, :description_detail_id, :status, :price, :quantity)
+	end
+
 end
