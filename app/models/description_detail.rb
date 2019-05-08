cfcafbd6da42148496192298f2fccf3f99def853
hanguyen1997 <truongbahanguyen@gmail.com>
2019-05-08 15:23:44 +0700

    bat loi thanh toan va bat loi ngay khoi hanh tour

--- app/models/description_detail.rb
+++ app/models/description_detail.rb
@@ -1,5 +1,22 @@
 class DescriptionDetail < ApplicationRecord
   belongs_to :tour
+  has_many :bookings
+
+  validates :price, presence: true,
+    numericality: {greater_than_or_equal_to: 1}
+  validates :start_day, presence: true
+  validates :end_day, presence: true
+  validate :check_day, on: [:update, :create]
 
   scope :desription_detail_by_tour, ->(tour_id){where tour_id: tour_id}
+  scope :hidden_expired_detail, ->{where "start_day > ?", Date.today}
+
+  def check_day
+    if start_day.present? && end_day.present?
+      if start_day > end_day || Date.today > start_day
+        errors.add :error, I18n.t("error_check_day")
+      end
+    end
+  end
+
 end
