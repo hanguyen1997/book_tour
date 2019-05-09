24ca233f0e2e2ac6184cf4e39c5d8307059c4c14
hanguyen1997 <truongbahanguyen@gmail.com>
2019-05-09 15:35:35 +0700

    chinh sua vai loi giao dien

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,22 +5,17 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def logged_in_user
-    # return if logged_in?
     return if user_signed_in?
     store_location
-    flash[:danger] = "Please log in"
+    flash[:danger] = "Vui Long Dang Nhap"
     redirect_to new_user_session_path
   end
 
   protected
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:registration, keys: [:name, :email,:password, :password_confirmation, :address, :phone_number])
-  # end
-
   def configure_permitted_parameters
 	devise_parameter_sanitizer.permit(:sign_up) do |u|
-	  u.permit :name, :email,:password, :password_confirmation, :address, :phone_number
+	  u.permit :name, :email,:password, :password_confirmation, :address, :phone_number, :role
 	end
 
 	devise_parameter_sanitizer.permit(:account_update) do |u|
